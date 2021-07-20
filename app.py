0f38931a805cd71b634c59ecb692bacbfa4f61b5
ladynsnoopy <myat005@ntu.edu.sg>
2021-07-20 19:25:28 +0800

    db.create tables problem

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
+
 
 app=Flask(__name__)
 #_--------------------------------------gonna be at root folder-------
@@ -15,10 +15,6 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'myat'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    # creates the table if they don't exist
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)  # jwt creates new endpoint /auth
 
@@ -27,6 +23,14 @@ api.add_resource(Item,'/Item/<string:name>')
 api.add_resource(ItemList,'/Items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(StoreList, '/stores')
+
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
